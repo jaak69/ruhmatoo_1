f21a31f63449991aa1f128107e8035819e62121e
jaakivask <jaak@adplus.ee>
2021-03-24 14:58:45 +0200

    jaak  muudatused

--- src/KuvaElektriHind.java
+++ src/KuvaElektriHind.java
@@ -1,34 +1,40 @@
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 public class KuvaElektriHind {
 
     private JSONObject jsonElering;
+    private JSONArray maaEE;
 
-    public void loeJson(String maa){
-        //JSONObject dataMaa = (JSONObject) jsonElering.get("ee");      // 2021-03-2
-        JSONArray dataMaa = (JSONArray) jsonElering.get("ee");
 
-        System.out.println("See peaks olema eesti: " + dataMaa);
-
-        }
 
 
     public void kuvaHomseElektriHinnaTabel(){
     }
 
-    public void määratudVahemikuMinMax(JSONObject statesJson, String maa){
-        //System.out.println(JSONObject jsonElering);
-        //Kuvab valitud perioodil 3 min hinda koos kuupäevaga ja 3 max hinda koos kuuupäevaga
+    public void määratudVahemikuMinMax(JSONObject statesJson, String riik){
+        
+        maaEE = loeJson(statesJson,riik);
 
+        for (Object aeg: maaEE) {
+
+            //töötle hind (see peaks olema privaatne meetod, sest käib kõigi andmete tõmbamise variantide kohta
+
+            //töötle timestamp, mida me siit tahame
+
+            //Min, Max väärtused
+
+        }
+
+        //kas see meetod tagastab väärtused või prindib lõpptulemuse välja?
     }
 
     public void kuvaSuvalineElektriHind(){
 
+    }
+
+    private JSONArray loeJson(JSONObject statesJson, String riik){
+       return (JSONArray) jsonElering.get(riik);
     }
 
 }
