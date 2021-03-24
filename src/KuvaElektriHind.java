f87f477307eae580148128edbe4392acb215701f
zaiksb <peeter.niidas@ut.ee>
2021-03-24 21:02:37 +0200

    JSON andmed loetud, TOP 3 arvutatud

--- src/KuvaElektriHind.java
+++ src/KuvaElektriHind.java
@@ -2,7 +2,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.*;
 
 public class KuvaElektriHind {
 
@@ -10,7 +10,13 @@ public class KuvaElektriHind {
     private JSONArray maaEE;
 
 
+    Elektrihind eesti = new Elektrihind();
 
+    // List elektritarbimise andmetega
+    private ArrayList<Elektrihind> elektrihind = new ArrayList<>();
+
+    // List top 3 salvestamiseks
+    private ArrayList<Elektrihind> tipud = new ArrayList<>();
 
     public void kuvaHomseElektriHinnaTabel(){
     }
@@ -20,6 +26,11 @@ public class KuvaElektriHind {
         //maaEE = loeJson(statesJson,riik);                           //SEE KUTSUB LÕPLIKU loeJson meetodi
         loeJson(statesJson, riik);
 
+        // Vaatan, kas midagi jäi üldse meelde
+        System.out.println(elektrihind.size());
+
+        // Leian top 3 elektrihinnad
+        System.out.println(top(3));
         //for (Object aeg: maaEE) {
 
             //töötle hind (see peaks olema privaatne meetod, sest käib kõigi andmete tõmbamise variantide kohta
@@ -42,17 +53,30 @@ public class KuvaElektriHind {
         JSONArray dataRiik = (JSONArray) statesJson.get(riik);
         for (int i = 0; i < dataRiik.size();i++){
             JSONObject tunniInfo = (JSONObject) dataRiik.get(i);
-            //String tund = tunnidTimestampist((String) tunniInfo.get("timestamp"));
-            //String tund = (String) tunniInfo.get("timestamp");
-            //String hind = (String) tunniInfo.get("price");
-            System.out.println("Hind " + tunniInfo.get("price") + " ajal " + tunniInfo.get("timestamp"));
-
+            String aeg = tunnidTimestampist((Long) tunniInfo.get("timestamp"));
+            double hind = Math.round(((double) tunniInfo.get("price"))/10.0*100)/100.0;
+            //System.out.println("Hind " + aeg +  " on " + hind + " senti/kWh");
+            Elektrihind tunnihind = new Elektrihind(aeg,hind);
+            elektrihind.add(tunnihind);
         }
+        //System.out.println(eesti.);
         //return (JSONArray) jsonElering.get(riik);                     SEE PEAB LÕPLIKUL OLEMA
     }
 
-    private String tunnidTimestampist (String timestamp){
-        return new SimpleDateFormat("H").format(new Date(Integer.parseInt(timestamp)*1000L));
+    // Elektri top'i arvutamine
+    private ArrayList<Elektrihind> top (int n){
+        Collections.sort(elektrihind, Collections.reverseOrder());
+        if (n > elektrihind.size()){
+            n = elektrihind.size();
+        }
+        for (int i = 0; i < n; i++){
+            tipud.add(elektrihind.get(i));
+        }
+        return tipud;
+    }
+
+    private String tunnidTimestampist (Long timestamp){
+        return new SimpleDateFormat("dd-MM-yyyy hh:mm").format(new Date((timestamp)*1000L));
     }
 
 }
